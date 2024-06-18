0f31bc99aeaefe75600e9dc4a7dfe48e13ae22be
DESKTOP-0LVN62V\Modern <askari.zahra94@gmail.com>
2024-06-18 11:22:31 -0500

    updated some api for front

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -3,29 +3,80 @@ const Transaction = require('../models/Transaction');
 exports.getTransactions = async (req, res) => {
     try {
         const { startDate, endDate } = req.query;
-        const startTimestamp = Date.parse(startDate);
-        const endTimestamp =Date.parse(endDate);
-        const transactions = await Transaction.find({
-            date: { $gte: startTimestamp, $lte: endTimestamp } ,
-            status : { $in: ["COMPLETED", "IN PROGRESS", "REJECTED"] } 
-        }).sort ({date:1});
-        const formattedTransactions = transactions.map(transaction => ({
-            ...transaction._doc,
-            date: new Date(transaction.date).toISOString().slice(0, 10)
-        }));
-        res.json(formattedTransactions);
+        let query = {};
+        query.status = {
+            $in: ["COMPLETED", "IN PROGRESS", "REJECTED"]
+          };
+        if (startDate && endDate) {
+            query.date = {
+              $gte: new Date(startDate).getTime(),
+              $lte: new Date(endDate).getTime()
+            };
+        }
+
+        const transactions = await Transaction.find(query).sort({date:1});
+        
+        let result = [];
+        transactions.forEach(transaction => {
+            result.push ({
+                id:transaction.id,
+                date:new Date(transaction.date).toISOString().slice(0, 10),
+                comments: transaction.comments
+            })
+        })
+        res.status(200).json(result);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 };
+
+exports.detailTransaction = async (req, res) => {
+    try {
+        const transactionId = req.body.id;
+        const transaction = await Transaction.findOne({id:transactionId});
+       
+        res.status(200).json({
+            id:transaction.id,
+            date: await this.formatDate(transaction.date),
+            comments:transaction.comments
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.updateComments = async (req,res) => {
+    try {
+        const transactionId = req.body.id;
+        const comments = req.body.comments;
+        await Transaction.updateOne(
+            { id: transactionId },
+            {comments:comments}
+          );
+       
+        res.status(200).json({
+            message : "transaction updated successfully"
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
 
 exports.loadTransactions = async (req, res) => {
    
     try {
         const data = req.body.data;
         await Transaction.insertMany(data);
-        res.status(201).json({ message: 'Transactions loaded successfully',});
+        res.status(200).json({ message: 'Transactions loaded successfully',});
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 };
+
+exports.formatDate = async (timestamp)=> {
+    const date = new Date(timestamp);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+}
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,10 @@
 const express=require('express');
-const { getTransactions, loadTransactions} = require('../controllers/transaction');
+const { getTransactions, loadTransactions, detailTransaction, updateComments} = require('../controllers/transaction');
 const router=express.Router();
 
-router.get('/transaction',getTransactions);
+router.get('/transactions',getTransactions);
 router.post('/transaction/load',loadTransactions);
+router.post('/detail-transaction',detailTransaction);
+router.put('/update-comment',updateComments);
 
 module.exports = router;
